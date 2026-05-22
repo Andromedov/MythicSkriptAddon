2e445b637c56a4d364a3e73be526509d6cf0cfe7
Andromedov <andromedov.mod@gmail.com>
2026-05-23 01:23:49 +0300

    [feature] introduce `MythicSkriptModule` integrating MythicMobs with custom conditions, effects, and expressions

--- src/main/java/com/gmail/berndivader/mythicskript/MythicSkript.java
+++ src/main/java/com/gmail/berndivader/mythicskript/MythicSkript.java
@@ -1,42 +1,51 @@
 package com.gmail.berndivader.mythicskript;
 
+import java.util.logging.Logger;
+
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.gmail.berndivader.mythicskript.classes.MythicClasses;
-import com.gmail.berndivader.mythicskript.conditions.Conditions;
-import com.gmail.berndivader.mythicskript.effects.Effects;
-import com.gmail.berndivader.mythicskript.events.Events;
-import com.gmail.berndivader.mythicskript.expressions.Expressions;
-import com.gmail.berndivader.mythicskript.functions.Functions;
-
-import ch.njol.skript.Skript;
+import org.skriptlang.skript.addon.SkriptAddon;
 
 public class MythicSkript extends JavaPlugin {
 
 	public static Plugin plugin;
-	
+	public static SkriptAddon addon;
+
+	/**
+	 * Initializes plugin; validates dependencies; registers Skript addon
+	 */
 	@Override
 	public void onEnable() {
-		plugin=this;
-		if(Bukkit.getPluginManager().getPlugin("MythicMobs")!=null&&Bukkit.getPluginManager().getPlugin("Skript")!=null){
-			
-			Skript.registerAddon(this);
-			
-			MythicClasses.register();
-			Events.register();
-			Conditions.register();
-			Expressions.register();
-			Effects.register();
-			Functions.register();
+		plugin = this;
+		Logger log = Bukkit.getLogger();
+
+		Plugin mm = Bukkit.getPluginManager().getPlugin("MythicMobs");
+		Plugin sk = Bukkit.getPluginManager().getPlugin("Skript");
+
+		if (mm == null || !mm.isEnabled()) {
+			log.severe("[MythicSkriptAddon] MythicMobs not found or not enabled - addon disabled.");
+			Bukkit.getPluginManager().disablePlugin(this);
+			return;
 		}
+		if (sk == null || !sk.isEnabled()) {
+			log.severe("[MythicSkriptAddon] Skript not found or not enabled - addon disabled.");
+			Bukkit.getPluginManager().disablePlugin(this);
+			return;
+		}
+
+		addon = ch.njol.skript.Skript.instance().registerAddon(MythicSkript.class, "MythicSkriptAddon");
+
+		addon.loadModules(new MythicSkriptModule());
+
+		log.info("[MythicSkriptAddon] Successfully loaded");
 	}
-	
+
 	@Override
 	public void onDisable() {
-		Bukkit.getLogger().info("MythicSk disabled!");
-		MythicSkript.plugin = null;
+		Bukkit.getLogger().info("[MythicSkriptAddon] Disabled.");
+		plugin = null;
+		addon  = null;
 	}
-	
 }
