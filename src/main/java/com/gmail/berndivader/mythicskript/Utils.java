328db902c3b22669fd057ccacc66bb9d0a62c7f3
Andromedov <andromedov.mod@gmail.com>
2026-05-22 20:43:47 +0300

    [refactor] encapsulate `Utils` initialization with `init()` method and improve code readability

--- src/main/java/com/gmail/berndivader/mythicskript/Utils.java
+++ src/main/java/com/gmail/berndivader/mythicskript/Utils.java
@@ -10,39 +10,42 @@ import io.lumine.mythic.core.mobs.MobExecutor;
 import io.lumine.mythic.core.skills.SkillTargeter;
 import io.lumine.mythic.core.spawning.spawners.SpawnerManager;
 
-public
-class 
-Utils
-{
-	public static MythicBukkit mythicMobs;
-	public static MobExecutor mobManager;
-	public static ItemExecutor itemManager;
-	public static BukkitAPIHelper mythicHelper;
-	public static DropExecutor dropExecutor;
-	public static SpawnerManager spawnerManager;
+public class Utils {
+
+	// Поля ініціалізуються один раз через init()
+	public static MythicBukkit        mythicMobs;
+	public static MobExecutor         mobManager;
+	public static ItemExecutor        itemManager;
+	public static BukkitAPIHelper     mythicHelper;
+	public static DropExecutor        dropExecutor;
+	public static SpawnerManager      spawnerManager;
 	public static VolatileCodeHandler VCH;
-	
-	static {
-		mythicMobs=MythicBukkit.inst();
-		mobManager=mythicMobs.getMobManager();
-		itemManager=mythicMobs.getItemManager();
-		mythicHelper=mythicMobs.getAPIHelper();
-		dropExecutor=mythicMobs.getDropManager();
-		VCH=mythicMobs.getVolatileCodeHandler();
-	}
-	
+
+	private Utils() {}
+
 	/**
-	 * 
-	 * @param targeter_string {@link String}
-	 * @return skill_targeter {@link SkillTargeter}
-	 * 
+	 * Initializes static managers and helpers for MythicMobs integration
 	 */
-	
-	public static SkillTargeter parseSkillTargeter(String targeter_string) {
-        String search = targeter_string.substring(1);
-        MythicLineConfigImpl mlc = new MythicLineConfigImpl(search);
-        String name = search.contains("{") ? search.substring(0, search.indexOf("{")) : search;
-        return mythicMobs.getSkillManager().getTargeter(name, mlc);
+	public static void init() {
+		mythicMobs     = MythicBukkit.inst();
+		mobManager     = mythicMobs.getMobManager();
+		itemManager    = mythicMobs.getItemManager();
+		mythicHelper   = mythicMobs.getAPIHelper();
+		dropExecutor   = mythicMobs.getDropManager();
+		spawnerManager = mythicMobs.getSpawnerManager();
+		VCH            = mythicMobs.getVolatileCodeHandler();
 	}
 
+
+	/**
+	 * Parses targeter string and retrieves configured skill targeter
+	 * @param targeterString - formatted string with @ and the name of the targeter (ex. @PIR{r=30})
+	 * @return - configured SkillTargeter or null
+	 */
+	public static SkillTargeter parseSkillTargeter(String targeterString) {
+		String search = targeterString.startsWith("@") ? targeterString.substring(1) : targeterString;
+		MythicLineConfigImpl mlc  = new MythicLineConfigImpl(search);
+		String name = search.contains("{") ? search.substring(0, search.indexOf('{')) : search;
+		return mythicMobs.getSkillManager().getTargeter(name, mlc);
+	}
 }
