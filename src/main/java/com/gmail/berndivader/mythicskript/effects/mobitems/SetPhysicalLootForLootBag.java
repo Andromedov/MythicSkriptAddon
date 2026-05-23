f08763250cc9ef483fc190a946b80052c2e81dce
Andromedov <andromedov.mod@gmail.com>
2026-05-23 11:15:13 +0300

    [refactor] improve `SetPhysicalLootForLootBag` logic by simplifying item drop handling and enhancing null safety

--- src/main/java/com/gmail/berndivader/mythicskript/effects/mobitems/SetPhysicalLootForLootBag.java
+++ src/main/java/com/gmail/berndivader/mythicskript/effects/mobitems/SetPhysicalLootForLootBag.java
@@ -12,11 +12,12 @@ import ch.njol.skript.lang.Effect;
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
+import io.lumine.mythic.api.adapters.AbstractItemStack;
+import io.lumine.mythic.api.drops.DropMetadata;
+import io.lumine.mythic.api.drops.IItemDrop;
 import io.lumine.mythic.bukkit.BukkitAdapter;
-import io.lumine.mythic.bukkit.adapters.BukkitItemStack;
 import io.lumine.mythic.core.drops.Drop;
 import io.lumine.mythic.core.drops.LootBag;
-import io.lumine.mythic.core.drops.droppables.ItemDrop;
 
 public class SetPhysicalLootForLootBag extends Effect {
 	Expression<LootBag> exprBag;
@@ -25,40 +26,52 @@ public class SetPhysicalLootForLootBag extends Effect {
 	@SuppressWarnings("unchecked")
 	@Override
 	public boolean init(Expression<?>[] expr, int arg1, Kleenean arg2, ParseResult arg3) {
-		if(expr.length>1) {
-			exprItem=(Expression<?>)expr[1]!=null?(Expression<ItemStack>)expr[1]:(Expression<ItemStack>)expr[2];
+		if (expr.length > 1) {
+			exprItem = expr[1] != null
+					? (Expression<ItemStack>) expr[1]
+					: (Expression<ItemStack>) expr[2];
 		}
-		exprBag=(Expression<LootBag>)expr[0];
+		exprBag = (Expression<LootBag>) expr[0];
 		return true;
 	}
 
 	@Override
 	public String toString(@Nullable Event e, boolean arg1) {
-		return getClass().getSimpleName()+e!=null?"@"+e.getEventName():"";
+		return getClass().getSimpleName() + (e != null ? "@" + e.getEventName() : "");
 	}
 
 	@Override
 	protected void execute(Event event) {
-		LootBag lootBag=exprBag.getSingle(event);
-		List<Drop>newDrops=new ArrayList<>();
-		if(exprItem!=null) {
-			if(exprItem.isSingle()) {
-				BukkitItemStack bit=(BukkitItemStack)BukkitAdapter.adapt(exprItem.getSingle(event));
-				ItemDrop drop=new ItemDrop("MMSK_DROP",null,bit);
-				drop.setAmount(1);
-				newDrops.add(drop);
+		LootBag lootBag = exprBag.getSingle(event);
+		if (lootBag == null) return;
+
+		List<Drop> newDrops = new ArrayList<>();
+		if (exprItem != null) {
+			if (exprItem.isSingle()) {
+				AbstractItemStack item = BukkitAdapter.adapt(exprItem.getSingle(event));
+				newDrops.add(new SimpleItemDrop(item));
 			} else {
-				ItemStack[]items=(exprItem.getArray(event));
-				int size=items.length;
-				for(int i1=0;i1<size;i1++) {
-					BukkitItemStack bit=(BukkitItemStack)BukkitAdapter.adapt(items[i1]);
-					ItemDrop drop=new ItemDrop("MMSK_DROP",null,bit);
-					drop.setAmount(1);
-					newDrops.add(drop);
+				for (ItemStack stack : exprItem.getArray(event)) {
+					newDrops.add(new SimpleItemDrop(BukkitAdapter.adapt(stack)));
 				}
 			}
 		}
 		lootBag.setLootTable(newDrops);
 	}
 
+	private static class SimpleItemDrop extends Drop implements IItemDrop {
+
+		private final AbstractItemStack item;
+
+		SimpleItemDrop(AbstractItemStack item) {
+			super("MMSK_DROP", null);
+			this.item = item;
+			this.setAmount(1);
+		}
+
+		@Override
+		public AbstractItemStack getDrop(DropMetadata data, double amount) {
+			return item;
+		}
+	}
 }
